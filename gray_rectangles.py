f9b14948e0fd0ed4adebcc479f49faf0048bc518
mixxxxx <spartacusxxxxxx@gmail.com>
2018-05-04 22:47:11 +0300

    first commit

--- gray_rectangles.py
+++ gray_rectangles.py
@@ -11,11 +11,81 @@ import numpy as np
 
 
 
+def threshold_slow(T, image):
+	# grab the image dimensions
+	h = image.shape[0]
+	w = image.shape[1]
+	
+	# loop over the image, pixel by pixel
+	for y in range(0, h):
+		for x in range(0, w):
+			# threshold the pixel
+			image[y, x] = 255 if image[y, x] >= T else 0
+			
+	# return the thresholded image
+	return image
+
+
+
+
 if __name__ == "__main__":
 
 
 	path_file = "indexc.jpeg"
 
+
+	img = cv2.imread(path_file)
+
+	cvt_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+	#cv2.imwrite('indexc_gray.jpeg', img)
+
+
+	cvt_image = threshold_slow(127, cvt_image)
+
+
+
+	cv2.imshow('image', cvt_image)
+
+
+
+	k = cv2.waitKey(0)
+	if k == 27:         # wait for ESC key to exit
+		cv2.destroyAllWindows()
+
+
+
+
+
+	# raw_image = cv2.imread(path_file)
+	# cv2.imshow('raw image',raw_image) 
+
+	# #get the size of pic(row & column)
+	# rows = raw_image.shape[0]
+	# cols = raw_image.shape[1] 
+
+	# #create a null array and be filled in with random number
+	# image = np.zeros(shape=(rows,cols,3), dtype=np.uint8) 
+	# for r in range(rows): 
+	# 	for c in range(cols):
+	# 		#print(image[r, c, 0])
+	# 		print(image[r, c, 1])
+	# 		break
+	# #         image[r, c, 0] = np.random.randint(0, 255)
+	# #         image[r, c, 1] = np.random.randint(0, 255)
+	# #         image[r, c, 2] = np.random.randint(0, 255)
+
+	# # cv2.imshow('random pixel image', image)
+	
+
+	# while (True):
+	# 	k=cv.waitKey(0)
+	# 	input('')
+	# 	if k==ord('e'):
+	# 		break
+
+	# cv2.destroyAllWindows()
+
+ 
 	#img = cv2.imread(path_file)
 	#img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 	#cv2.imwrite('indexc_gray.jpeg', img)
@@ -56,39 +126,40 @@ if __name__ == "__main__":
 
 
 
-	photopath = 'indexc.jpeg'
-	classifier = os.getcwd()+'/haarcascade_frontalface_default.xml'
+	# photopath = 'indexc.jpeg'
+	# classifier = os.getcwd()+'/haarcascade_frontalface_default.xml'
+
+ 
+
+	# image = cv2.imread(photopath)
 
 
-	image = cv2.imread(photopath)
+	# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# face_casacade = cv2.CascadeClassifier(classifier)
 
 
-	face_casacade = cv2.CascadeClassifier(classifier)
+	# faces = face_casacade.detectMultiScale(image)
 
 
-	faces = face_casacade.detectMultiScale(image)
-
-
-	color = (0,0,255)
-	strokeWeight = 1
+	# color = (0,0,255)
+	# strokeWeight = 1
 	
-	windowName = "Object Detection"
+	# windowName = "Object Detection"
 
-	while True: 
+	# while True: 
 
-		print(len(faces))
-		for x, y, width, height in faces:
-			cv2.rectangle(image, (x, y), (x + width, y + height), color, strokeWeight)
+	# 	print(len(faces))
+	# 	for x, y, width, height in faces:
+	# 		cv2.rectangle(image, (x, y), (x + width, y + height), color, strokeWeight)
 
 	
-		cv2.imshow(windowName, image)
+	# 	cv2.imshow(windowName, image)
 
 		
-		if cv2.waitKey(20) == 27:
-			break
+	# 	if cv2.waitKey(20) == 27:
+	# 		break
 
 	
-	exit()
+	# exit()
